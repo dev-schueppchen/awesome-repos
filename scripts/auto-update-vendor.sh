6572e2fad4744ea22e0fe65e97552d911d2e1b36
Ringo Hoffmann <riho@live.de>
2019-02-15 10:34:10 +0100

    added auto update script

---
+++ scripts/auto-update-vendor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPOURL="git@github.com:dev-schueppchen/awesome-repos.git"
+REPODIR="./.aweome-repos"
+
+git clone $REPOURL $REPODIR
+git -C $REPODIR git submodule update --recursive
+git -C $REPODIR add .
+git -C $REPODIR -c user.name="auto-vendor-update" -c user.email="auto-vendor-update" commit -m "automatically updated submodules"
+git -C $REPODIR push
+rm -r -f $REPODIR
